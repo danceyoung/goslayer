2582ce7396ef283aa68ad07acdbe390ccbc8afb0
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-22 23:05:16 +0800

    add template

--- internal/goslayer/layer/template.go
+++ internal/goslayer/layer/template.go
@@ -1,11 +1,53 @@
 package layer
 
-func mainTemplate() string {
+func mainUseHttpHandlerTemplate() string {
 	return `package main
 
-import "fmt"
+import (
+	_ "goslayer/cmd/myapp/router"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World.")
+	log.Println(http.ListenAndServe(":8080", nil))
+}`
+}
+
+func routerTemplate() string {
+	return `package router
+
+import (
+	"goslayer/cmd/myapp/router/handler"
+	"net/http"
+)
+
+func init() {
+	path := "/goslayer"
+	eh := new(handler.EventHandler)
+	http.Handle(path+"/events", http.HandlerFunc(eh.Events))
+}`
+}
+
+func baseHandlerTemplate() string {
+	return `package handler
+
+type BaseHandler struct{}`
+}
+
+func eventHandlerTemplate() string {
+	return `package handler
+
+import (
+	"io"
+	"net/http"
+)
+
+type EventHandler struct {
+	BaseHandler
+}
+
+func (eventh *EventHandler) Events(rw http.ResponseWriter, req *http.Request) {
+	io.WriteString(rw, "Hello World")
 }`
 }
