de4f8673543cfefe155d1a30d43f0b3b1f1f15ca
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-22 23:06:01 +0800

    add choose webframe step and update layer state pattern

--- internal/goslayer/layer/layer.go
+++ internal/goslayer/layer/layer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type Step interface {
@@ -14,35 +15,78 @@ type DescStep struct {
 }
 
 func (descstep DescStep) Do(layer *Layer) {
-	if strings.TrimSpace(layer.steptext) == "" {
+	if layer.textscanned == "" {
 		fmt.Println("GoSLayer is a tool that helps you to create a golang project that is layered base on a standard architecture layout and followed by Package-Oriented-Design guideline.")
 		fmt.Print("Please enter your project name: ")
 	} else {
-		layer.NextStepAndDo(CreateStep{})
+		layer.projectname = layer.textscanned
+		fmt.Println("Please choose a web framework: ")
+		fmt.Println("[1] use gin")
+		fmt.Println("[2] use http's handler buildin golang")
+		fmt.Print("entry 1 or 2: ")
+		layer.next(ChooseWebFStep{})
 	}
+}
+
+type ChooseWebFStep struct {
+}
+
+func (choosestep ChooseWebFStep) Do(layer *Layer) {
+	layer.webframework = layer.textscanned
+	if layer.webframework == "1" {
+		fmt.Println("Creating your go project with GIN")
+	} else if layer.webframework == "2" {
+		fmt.Println("Creating your go project with http.Handler")
+	} else {
+		fmt.Println("Creating your go project without any web framework.")
+	}
+	time.Sleep(time.Second * 1)
+	layer.next(CreateStep{})
+	layer.do()
 }
 
 type CreateStep struct{}
 
 func (createstep CreateStep) Do(layer *Layer) {
-	projectName := layer.steptext
-	os.Mkdir("./"+projectName, os.ModePerm)
-	os.MkdirAll("./"+projectName+"/cmd/myapp", os.ModePerm)
-	os.MkdirAll("./"+projectName+"/cmd/myapp/router/handler", os.ModePerm)
-	mainfile, err := os.Create("./" + projectName + "/cmd/myapp/main.go")
+	os.Mkdir("./"+layer.projectname, os.ModePerm)
+	os.MkdirAll("./"+layer.projectname+"/cmd/myapp", os.ModePerm)
+	os.MkdirAll("./"+layer.projectname+"/cmd/myapp/router/handler", os.ModePerm)
+	routerfile, err := os.Create("./" + layer.projectname + "/cmd/myapp/router/router.go")
 	if err != nil {
-		fmt.Println("Raise a error when creating project: ", err.Error())
+		fmt.Println("panic a error when creating project: ", err.Error())
 		panic(nil)
 	}
+	if layer.webframework == "2" {
+		routerfile.WriteString(strings.ReplaceAll(routerTemplate(), "goslayer", layer.projectname))
+		basehanderfile, err := os.Create("./" + layer.projectname + "/cmd/myapp/router/handler/basehandler.go")
+		if err != nil {
+			fmt.Println("panic a error when creating project: ", err.Error())
+			panic(nil)
+		}
+		basehanderfile.WriteString(baseHandlerTemplate())
 
-	mainfile.WriteString(mainTemplate())
+		eventhanderfile, err := os.Create("./" + layer.projectname + "/cmd/myapp/router/handler/eventhandler.go")
+		if err != nil {
+			fmt.Println("panic a error when creating project: ", err.Error())
+			panic(nil)
+		}
+		eventhanderfile.WriteString(eventHandlerTemplate())
 
-	os.MkdirAll("./"+projectName+"/internal/myapp", os.ModePerm)
-	os.Mkdir("./"+projectName+"/internal/pkg", os.ModePerm)
+		mainfile, err := os.Create("./" + layer.projectname + "/cmd/myapp/main.go")
+		if err != nil {
+			fmt.Println("panic a error when creating project: ", err.Error())
+			panic(nil)
+		}
+		mainfile.WriteString(strings.ReplaceAll(mainUseHttpHandlerTemplate(), "goslayer", layer.projectname))
+	}
 
-	os.Mkdir("./"+projectName+"/pkg", os.ModePerm)
+	os.MkdirAll("./"+layer.projectname+"/internal/myapp", os.ModePerm)
+	os.Mkdir("./"+layer.projectname+"/internal/pkg", os.ModePerm)
 
-	layer.NextStepAndDo(OverStep{})
+	os.Mkdir("./"+layer.projectname+"/pkg", os.ModePerm)
+
+	layer.next(OverStep{})
+	layer.do()
 }
 
 type OverStep struct{}
@@ -53,20 +97,25 @@ func (overstep OverStep) Do(layer *Layer) {
 }
 
 type Layer struct {
-	step     Step
-	steptext string
+	step         Step
+	textscanned  string
+	projectname  string
+	webframework string
 }
 
 func NewLayer(cstep Step) *Layer {
 	return &Layer{step: cstep}
 }
 
-func (layer *Layer) NextStepAndDo(cstep Step) {
+func (layer *Layer) next(cstep Step) {
 	layer.step = cstep
+}
+
+func (layer *Layer) do() {
 	layer.step.Do(layer)
 }
 
-func (layer *Layer) JustDo(steptext string) {
-	layer.steptext = steptext
+func (layer *Layer) JustDo(textscanned string) {
+	layer.textscanned = strings.TrimSpace(textscanned)
 	layer.step.Do(layer)
 }
