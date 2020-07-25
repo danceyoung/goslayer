608f2a49114804d78020466681472af3a7c5f0e1
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-25 15:45:50 +0800

    use httpset middleware

--- cmd/myapp/router/router.go
+++ cmd/myapp/router/router.go
@@ -4,10 +4,11 @@ import (
 	"net/http"
 
 	"github.com/danceyoung/goslayer/cmd/myapp/router/handler"
+	"github.com/danceyoung/goslayer/internal/pkg/middleware"
 )
 
 func init() {
 	path := "/goslayer"
 	eh := new(handler.EventHandler)
-	http.Handle(path+"/events", http.HandlerFunc(eh.Events))
+	http.Handle(path+"/events", middleware.HttpSet((eh.Events)))
 }
