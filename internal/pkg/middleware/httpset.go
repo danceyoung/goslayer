c415151dc586123b20b25598c7e2a7610b20ce6c
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-25 15:45:38 +0800

    create httpset middleware

---
+++ internal/pkg/middleware/httpset.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func HttpSet(hf func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		log.Println("The url requesting is ", req.URL)
+		rw.Header().Set("Content-Type", "application/json")
+		hf(rw, req)
+	})
+}
