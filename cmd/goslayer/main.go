94bf22cdd599ac8065498f1aed9c272dcbd4bcf2
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-25 16:41:57 +0800

    add desc statement

--- cmd/goslayer/main.go
+++ cmd/goslayer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/danceyoung/goslayer/internal/goslayer/layer"
@@ -13,6 +14,8 @@ func main() {
 
 		}
 	}()
+
+	fmt.Println("\nGoSLayer is a tool that helps you to create a golang project in seconds.\n\n • layered base on a standard architecture layout\n • followed by Package-Oriented-Design guideline\n link: https://github.com/danceyoung/goslayer\n")
 
 	layer := layer.NewLayer(layer.DescStep{})
 	layer.JustDo("")
