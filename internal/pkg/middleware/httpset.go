5ff021ec820e4d9e0f6c04a098663365b476072e
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-27 23:09:00 +0800

    Update httpset.go

--- internal/pkg/middleware/httpset.go
+++ internal/pkg/middleware/httpset.go
@@ -7,7 +7,7 @@ import (
 
 func HttpSet(hf func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		log.Println("The url requesting is ", req.URL)
+		log.Println(req.Method+" the url requesting is ", req.URL)
 		rw.Header().Set("Content-Type", "application/json")
 		hf(rw, req)
 	})
