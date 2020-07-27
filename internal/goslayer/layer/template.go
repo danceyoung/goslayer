18303bbd2a337a7388f050beb134bfe8e7ffe08a
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-27 23:21:46 +0800

    Update template.go

--- internal/goslayer/layer/template.go
+++ internal/goslayer/layer/template.go
@@ -47,15 +47,48 @@ func init() {
 func (hht httpHandlerTemplate) baseHandlerTemplate() string {
 	return `package handler
 
-type BaseHandler struct{}`
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type BaseHandler struct{}
+
+func (baseh *BaseHandler) responseOk(rw http.ResponseWriter, data interface{}) {
+	result := make(map[string]interface{})
+	result["code"] = 0
+	result["msg"] = "ok"
+	result["data"] = data
+	bytes, _ := json.Marshal(result)
+	io.WriteString(rw, string(bytes))
+}
+
+func (baseh *BaseHandler) recoverPanic(rw http.ResponseWriter) {
+	if err := recover(); err != nil {
+		baseh.responseError(rw, err.(error))
+	}
+}
+
+func (baseh *BaseHandler) responseError(rw http.ResponseWriter, err error) {
+	result := make(map[string]interface{})
+	result["code"] = -1
+	result["msg"] = err.Error()
+	bytes, _ := json.Marshal(result)
+	io.WriteString(rw, string(bytes))
+}`
 }
 
 func (hht httpHandlerTemplate) eventHandlerTemplate() string {
 	return `package handler
 
 import (
-	"io"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
+
+	"goslayer/internal/myapp/event"
 )
 
 type EventHandler struct {
@@ -63,7 +96,38 @@ type EventHandler struct {
 }
 
 func (eventh *EventHandler) Events(rw http.ResponseWriter, req *http.Request) {
-	io.WriteString(rw, "Hello World")
+	events, err := event.Events()
+	if err != nil {
+		eventh.responseError(rw, err)
+	}
+
+	eventh.responseOk(rw, events)
+}
+
+func (eventh *EventHandler) JoinAEvent(rw http.ResponseWriter, req *http.Request) {
+	defer eventh.recoverPanic(rw)
+	req.ParseForm()
+
+	bodybytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	if req.Method != http.MethodPost {
+		panic(errors.New("not matched handler"))
+	}
+
+	var m event.Member
+	err = json.Unmarshal(bodybytes, &m)
+	if err != nil {
+		panic(err)
+	}
+
+	err = event.JoinAEvent(req.Form.Get("event-id"), m)
+	if err != nil {
+		panic(err)
+	}
+
+	eventh.responseOk(rw, nil)
 }`
 }
 
@@ -77,7 +141,7 @@ import (
 
 func HttpSet(hf func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		log.Println("The url requesting is ", req.URL)
+		log.Println(req.Method+" the url requesting is ", req.URL)
 		rw.Header().Set("Content-Type", "application/json")
 		hf(rw, req)
 	})
