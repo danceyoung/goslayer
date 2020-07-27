9526e63ee9ee777f2cf6a16460f546d1fe65974b
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-27 23:08:40 +0800

    implement events and joinaevent handler

--- cmd/myapp/router/handler/eventhandler.go
+++ cmd/myapp/router/handler/eventhandler.go
@@ -1,8 +1,12 @@
 package handler
 
 import (
-	"io"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/danceyoung/goslayer/internal/myapp/event"
 )
 
 type EventHandler struct {
@@ -10,5 +14,36 @@ type EventHandler struct {
 }
 
 func (eventh *EventHandler) Events(rw http.ResponseWriter, req *http.Request) {
-	io.WriteString(rw, "Hello World")
+	events, err := event.Events()
+	if err != nil {
+		eventh.responseError(rw, err)
+	}
+
+	eventh.responseOk(rw, events)
+}
+
+func (eventh *EventHandler) JoinAEvent(rw http.ResponseWriter, req *http.Request) {
+	defer eventh.recoverPanic(rw)
+	req.ParseForm()
+
+	bodybytes, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	if req.Method != http.MethodPost {
+		panic(errors.New("not matched handler"))
+	}
+
+	var m event.Member
+	err = json.Unmarshal(bodybytes, &m)
+	if err != nil {
+		panic(err)
+	}
+
+	err = event.JoinAEvent(req.Form.Get("event-id"), m)
+	if err != nil {
+		panic(err)
+	}
+
+	eventh.responseOk(rw, nil)
 }
