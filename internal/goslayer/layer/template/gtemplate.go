906cb02836413e0cadd80f800eb566f0912bdfda
dingchaoyang <dingchaoyang@kingsoft.com>
2020-07-31 19:44:47 +0800

    Create gtemplate.go

---
+++ internal/goslayer/layer/template/gtemplate.go
@@ -0,0 +1,22 @@
+package template
+
+type ginTemplate struct{}
+
+func (gt ginTemplate) MainTemplate() string {
+	return ``
+}
+func (gt ginTemplate) RouterTemplate() string {
+	return ``
+}
+func (gt ginTemplate) BaseHandlerTemplate() string {
+	return ``
+}
+func (gt ginTemplate) EventHandlerTemplate() string {
+	return ``
+}
+func (gt ginTemplate) HttpMiddlewareTemplate() string {
+	return ``
+}
+func (gt ginTemplate) EventBizTemplate() string {
+	return ``
+}
