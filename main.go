0b1c684463ecdebd108548601d5f8c8271371571
dingchaoyang <dingchaoyang@kingsoft.com>
2020-08-05 14:02:51 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/danceyoung/goslayer/internal/goslayer/layer"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err == nil {
+
+		}
+	}()
+
+	fmt.Println("\nGoSLayer is a tool that helps you to create a golang project in seconds.\n\n • layered base on a standard architecture layout\n • followed by Package-Oriented-Design guideline\n link: https://github.com/danceyoung/goslayer\n")
+
+	layer := layer.NewLayer(layer.EntryProjectNameStep{})
+	layer.JustDo("")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		layer.JustDo(scanner.Text())
+	}
+}
